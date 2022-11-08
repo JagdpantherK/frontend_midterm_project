8d4f338a7512e699ce04873a626da3b2f64aea99
JagdpantherK <jagpanthertitnium@gmail.com>
2022-11-08 20:11:02 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import './App.css';
 import logo from './assets/logo.png'
 import Textarea from "./components/Textarea";
-
+const btn = document.querySelector("#button")
+btn.addEventListener("click", (where, element) =>{
+    console.log("Button clicked");
+    let text = document.querySelector("#textarea");
+    let result = document.querySelector("#text")
+    result.innerHTML = ceasar(text);
+});
+function ceasar(str){
+    let solved = "";
+    for (let i = 0; i < str.length; i++) {
+        let asciiNum = str[i].charCodeAt();
+        if (asciiNum >= 65 && asciiNum <= 90){
+            solved += String.fromCharCode(asciiNum + 13)
+        }
+        else if(asciiNum >= 78 && asciiNum <=90){
+            solved += String.fromCharCode(asciiNum - 13)
+        }
+        else {
+            solved = str[i];
+        }
+    }
+    return solved;
+}
 function App() {
     return (
         <div className="App">
